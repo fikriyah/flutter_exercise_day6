88bffd187b3677bac1888613a307fcb7b4bd17a1
fikriyah <iraafhs@gmail.com>
2021-11-03 09:52:06 +0800

    initial commit

--- lib/cubit/capcubit.dart
+++ lib/cubit/capcubit.dart
@@ -4,7 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Capitalize extends Cubit<String> {
-  Capitalize() : super("    ");
+  Capitalize() : super("");
 
   void capIt(String text) {
     emit(text.toUpperCase());
